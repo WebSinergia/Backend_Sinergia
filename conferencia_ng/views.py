f2e7917d6ac500affdcbad3dff3e2ca7a6ce7f47
Caleb <reateguicevallosc@gmail.com>
2024-08-31 11:50:00 -0500

    Fix: URL Parameter

--- conferencia_ng/views.py
+++ conferencia_ng/views.py
@@ -31,7 +31,7 @@ class UserRetrieveDNIView(generics.RetrieveAPIView):
 class UserUpdatePaymentView(generics.RetrieveUpdateAPIView):
     queryset = User.objects.all()
     serializer_class = UserSerializer
-    lookup_field = 'id'  
+    lookup_field = 'user_id'
 
     def update(self, request, *args, **kwargs):
         user = self.get_object()
